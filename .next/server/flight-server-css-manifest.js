393e40a8fc14fdf0e5e311aba36b0ac66e385f51
mikumuu <mikimuuu@gmail.com>
2023-11-11 22:21:03 +0900

    new

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,22 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/kimuramikihisa/Desktop/rei/app/layout.tsx": [
+      "/Users/kimuramikihisa/Desktop/rei/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/kimuramikihisa/Desktop/rei/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/kimuramikihisa/Desktop/rei/app/page": [
+      "/Users/kimuramikihisa/Desktop/rei/app/globals.css",
+      "/Users/kimuramikihisa/Desktop/rei/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/kimuramikihisa/Desktop/rei/app/posts/[...slug]/page": [
+      "/Users/kimuramikihisa/Desktop/rei/app/globals.css",
+      "/Users/kimuramikihisa/Desktop/rei/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/kimuramikihisa/Desktop/rei/app/[...slug]/page": [
+      "/Users/kimuramikihisa/Desktop/rei/app/globals.css",
+      "/Users/kimuramikihisa/Desktop/rei/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
